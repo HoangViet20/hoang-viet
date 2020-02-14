b05526a20437bc9a0f42cac1635ae19d5e7df0b0
HoangViet <godofdark12@gmail.com>
2020-02-14 20:46:17 +0700

    initial commit

---
+++ Homework/Homework 3/homework3.js
@@ -0,0 +1,37 @@
+// let a = 5
+// let b = 6
+// console.log(a, b)
+// b = [a, a = b][0]
+// console.log(a, b)
+
+
+// let a = 5
+// let b = 6
+// [a, b] = [b, a];
+// console.log(`${a} ${b}`);
+
+
+// const s = 'Hello beauty there'
+// const a = Array(s)
+// let res = s.split(" ")
+// console.log(res)
+
+
+// const a = [4, 5, 7, -8];
+// console.log(...a);
+
+
+let
+
+
+
+
+
+
+
+
+
+
+
+
+
